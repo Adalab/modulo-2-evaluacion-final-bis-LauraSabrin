a1a441195e2b1fd2389fbf0b37badd25d6a4b96c
LauraSabrin <waila_05@hotmail.com>
2024-01-26 19:45:49 +0100

    css

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@
 
 console.log('>> Ready :)');
 
+const allFriendSection = document.querySelector('.js-allFriends');
+const addFriendSection = document.querySelector('.js-addFriends');
+const saveBtn = document.querySelector('.js-saveBtn');
+const getBtn = document.querySelector('.js-getBtn');
