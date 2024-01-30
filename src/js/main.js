427e496a6ce0eb94385a8a0baedd5911fe848ace
LauraSabrin <waila_05@hotmail.com>
2024-01-30 21:38:50 +0100

    class and LS

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,10 @@ const addFriendSection = document.querySelector(".js-addFriends");
 const saveBtn = document.querySelector(".js-saveBtn");
 const getBtn = document.querySelector(".js-getBtn");
 const urlAllInfo = "https://randomuser.me/api/?results=10";
-const sectionUser = document.querySelector(".js-allUsers");
+let sectionUser = document.querySelector(".js-allUsers");
 
 let allInfoApi = [];
-let allInfoId = [];
+let friends = [];
 
 //función para pedir datos API
 const getDataUser = () => {
@@ -18,36 +18,15 @@ const getDataUser = () => {
     .then((response) => response.json())
     .then((data) => {
       allInfoApi = data.results;
-      console.log(allInfoApi);
-      renderTenUsers();
+      renderTenUsers(allInfoApi, sectionUser);
+      localStorage.setItem('users', JSON.stringify('allInfoApi'));
+      
     })
-
     .catch((error) => {
       console.error("Error en la petición fetch:", error);
     });
 };
 getDataUser();
-
-//función añadir clase para objeto con propiedad isFriend=true
-const friend = () => {
-  for (let i = 0; i <= allInfoId; i++) {
-    if (allInfoId.contains(isFriend)) {
-      //                                                                                 <------------------- AQUÍ !!
-    }
-  }
-};
-
-//función añadir propiedad nueva
-// const addProperty = () => {
-//   const newProperty = {
-//     isFriend: true,
-//   };
-
-//   for (let i = 0; i < allInfoId.length; i++) {
-//     allInfoId[i].isFriend = true;
-//     newProperty[allInfoId[i]];
-//   }
-// };
 
 //función para encontrar el id del elemento clicado y añadir propiedad isFriend:
 const handleFriend = (event) => {
@@ -61,22 +40,19 @@ const handleFriend = (event) => {
   );
   console.log(whoIsClicked);
   //con findIndex () busco y encuentro la posición o index de un elemento:
-  const indexUSer = allInfoApi.findIndex(
+  const indexUser = allInfoApi.findIndex(
     (user) => user.login.uuid === cardClickedId
   );
-  console.log(indexUSer);
+  console.log(indexUser);
   //si la posición de
-  if (indexUSer !== -1) {
-    allInfoApi[indexUSer].isFriend = true;
+  if (indexUser !== -1) {
+    allInfoApi[indexUser].isFriend = true;
     console.log(allInfoApi);
-    allInfoId.push(whoIsClicked); //debería ser whoIsClicked o cardClicked?
-    
- //                                                               <---------------------------------------AQUÍ!!!
-    renderTenUsers();   //si descomento y se ejecuta me multiplica los resultados con cada click.
+    friends.push(whoIsClicked);
+    console.log(friends);
   }
+  renderTenUsers(friends, sectionUser);
 };
-
-
 
 //función para escuchar evento sobre todos los articles (mis tarjetas de usuario)
 const FindId = () => {
@@ -86,46 +62,63 @@ const FindId = () => {
   }
 };
 
-//función para saber el elemento clicado
-// const handleClick =(event)=>{
-//   event.preventDefault();
-//  const cardClicked = event.currentTarget.id;
-//  console.log(cardClicked)
-//   const clicked = allInfoApi.map((user) => cardClicked === user.login.uuid);
-//   allInfoId.push(clicked);
-//   console.log(allInfoId);
-//     console.log(clicked);
-//     if ( cardClicked === allInfoId ) {
-//           addProperty();
-//           allCardUser.classList.add('isFriend');
-//         }
-//       }
-//  sectionUser.addEventListener('click', handleClick);
-
 //función actualizar-pintar en pantalla los 10 usuarios random
-const renderTenUsers = (arrayUsers) => {
-  //limpiar section ""
+const renderTenUsers = () => {
+  sectionUser.innerHTML = "";
   for (let i = 0; i < allInfoApi.length; i++) {
-    let friend = "";
-    // if (  )
-    sectionUser.innerHTML += `<article class="allUsersArt js-all-users-art" id="${
-      allInfoApi[i].login.uuid
-    }">
-  <figure class="allUsersArt__figallUsersArt__fig"><img class="allUsersArt__fig--img" src="${
-    allInfoApi[i].picture.medium
-  }" alt="imagen de usuario"/></figure>
-  <h3 class="allUsersArt__h3">  ${
-    allInfoApi[i].name.first + " " + allInfoApi[i].name.last
-  }</h3>
-  <p class="allUsersArt__p" > <img class="allUsersArt__fig--icon" src= "images/user.png" alt =""/> ${
-    allInfoApi[i].login.username
-  } </p>
-  <p class="allUsersArt__p" ><img class="allUsersArt__fig--icon" src= "images/home.png" alt =/> ${
-    allInfoApi[i].location.city
-  }</p>
-  <p class="allUsersArt__p" >${allInfoApi[i].location.country}</p>
-</article>`;
+    if (allInfoApi[i].isFriend) {
+      sectionUser.innerHTML += `<article class="allUsersArt isFriend  js-all-users-art" id="${
+        allInfoApi[i].login.uuid
+      }">
+        <figure class="allUsersArt__figallUsersArt__fig"><img class="allUsersArt__fig--img" src="${
+          allInfoApi[i].picture.medium
+        }" alt="imagen de usuario"/></figure>
+        <h3 class="allUsersArt__h3">  ${
+          allInfoApi[i].name.first + " " + allInfoApi[i].name.last
+        }</h3>
+        <p class="allUsersArt__p" > <img class="allUsersArt__fig--icon" src= "images/user.png" alt =""/> ${
+          allInfoApi[i].login.username
+        } </p>
+        <p class="allUsersArt__p" ><img class="allUsersArt__fig--icon" src= "images/home.png" alt =/> ${
+          allInfoApi[i].location.city
+        }</p>
+        <p class="allUsersArt__p" >${allInfoApi[i].location.country}</p>
+        </article>`;
+    } else {
+      sectionUser.innerHTML += `<article class="allUsersArt js-all-users-art" id="${
+        allInfoApi[i].login.uuid
+      }">
+          <figure class="allUsersArt__figallUsersArt__fig"><img class="allUsersArt__fig--img" src="${
+            allInfoApi[i].picture.medium
+          }" alt="imagen de usuario"/></figure>
+          <h3 class="allUsersArt__h3">  ${
+            allInfoApi[i].name.first + " " + allInfoApi[i].name.last
+          }</h3>
+          <p class="allUsersArt__p" > <img class="allUsersArt__fig--icon" src= "images/user.png" alt =""/> ${
+            allInfoApi[i].login.username
+          } </p>
+          <p class="allUsersArt__p" ><img class="allUsersArt__fig--icon" src= "images/home.png" alt =/> ${
+            allInfoApi[i].location.city
+          }</p>
+          <p class="allUsersArt__p" >${allInfoApi[i].location.country}</p>
+          </article>`;
+    }
   }
   FindId();
-  // handleClick();
 };
+
+getDataUser();
+const handleRecover =()=> {
+  const savedUsers = JSON.parse(localStorage.getItem('allInfoApi'));
+  if (savedUsers  !== null) {
+    allInfoApi = savedUsers;
+    renderTenUsers(savedUsers, allUserSection);
+  } else {
+    getDataUser();
+  }
+  console.log(allInfoApi);
+}
+handleRecover();
+
+saveBtn.addEventListener('click', handleSave);
+getBtn.addEventListener('click', handleRecover);
