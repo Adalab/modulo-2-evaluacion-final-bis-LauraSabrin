64f66400fc346ba0ea914db275e7aeb3de5230b8
LauraSabrin <waila_05@hotmail.com>
2024-01-31 14:42:43 +0100

    recover

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 "use strict";
 
-console.log(">> Ready :)");
 
-// const allUserSection = document.querySelector(".js-allUsers");
 const addFriendSection = document.querySelector(".js-addFriends");
 const saveBtn = document.querySelector(".js-saveBtn");
 const getBtn = document.querySelector(".js-getBtn");
@@ -18,42 +16,44 @@ const getDataUser = () => {
     .then((response) => response.json())
     .then((data) => {
       allInfoApi = data.results;
-      renderTenUsers(allInfoApi, sectionUser);
-      handleSave();
-    })
+      renderTenUsers();
+      })
     .catch((error) => {
       console.error("Error en la petición fetch:", error);
     });
 };
 getDataUser();
 
+//evento click en botón guardar y se almacena en LS los usuarios de pantalla.
+// 1-evento clic
+// 2-guardar en variable los usuarios : nombre variable = función con setItem?
 const handleSave = () => {
-  localStorage.setItem('users', JSON.stringify(allInfoApi));
+    localStorage.setItem("users", JSON.stringify(allInfoApi));
+
 };
-localStorage.setItem('nombre', 'Pablo');
 
 //función para encontrar el id del elemento clicado y añadir propiedad isFriend:
 const handleFriend = (event) => {
   const cardClicked = event.currentTarget;
-  console.log(cardClicked);
+  
   const cardClickedId = event.currentTarget.id;
-  console.log(cardClickedId);
+ 
   //con find() obtengo el primer elemento que cumple mi condición
   const whoIsClicked = allInfoApi.find(
     (user) => cardClickedId === user.login.uuid
   );
-  console.log(whoIsClicked);
+ 
   //con findIndex () busco y encuentro la posición o index de un elemento:
   const indexUser = allInfoApi.findIndex(
     (user) => user.login.uuid === cardClickedId
   );
-  console.log(indexUser);
+ 
   //si la posición de
   if (indexUser !== -1) {
     allInfoApi[indexUser].isFriend = true;
-    console.log(allInfoApi);
+  
     friends.push(whoIsClicked);
-    console.log(friends);
+    
   }
   renderTenUsers(friends, sectionUser);
 };
@@ -80,7 +80,7 @@ const renderTenUsers = () => {
         <h3 class="allUsersArt__h3">  ${
           allInfoApi[i].name.first + " " + allInfoApi[i].name.last
         }</h3>
-        <p class="allUsersArt__p" > <img class="allUsersArt__fig--icon" src= "images/user.png" alt =""/> ${
+        <p class="allUsersArt__p" > <img class="allUsersArt__fig--icon" src= "../images/user.png" alt =""/> ${
           allInfoApi[i].login.username
         } </p>
         <p class="allUsersArt__p" ><img class="allUsersArt__fig--icon" src= "images/home.png" alt =/> ${
@@ -110,23 +110,23 @@ const renderTenUsers = () => {
   }
   FindId();
 };
-getDataUser();
 
-const handleRecover = () => {
-  const savedUsers = JSON.parse(localStorage.getItem('users'));
+
+const handleRecover = (event) => {
+  event.preventDefault();
+  console.log('Me han clicado!');
+  const savedUsers = JSON.parse(localStorage.getItem("users"));
   console.log(savedUsers);
   if (savedUsers !== null) {
     allInfoApi = savedUsers;
     console.log(allInfoApi);
-    renderTenUsers(allInfoApi, sectionUser);
-  } else {
-    getDataUser();
-  }
-  
+    renderTenUsers();
+  } 
 };
-handleRecover();
 
-saveBtn.addEventListener("click", handleSave);
+
 getBtn.addEventListener("click", handleRecover);
+saveBtn.addEventListener("click", handleSave); 
 
+console.log('Guardaste!')
 
